603b7cee37d92752294d55db0594f94d9ab147d8
WindowsXpguy <theaverageuser404@gmail.com>
2025-03-13 06:42:05 -0400

    Update MultiButton_test.js

--- MultiButton_test.js
+++ MultiButton_test.js
@@ -3,14 +3,16 @@ function createMainButton() {
     var buttonClass = ModAPI.reflect.getClassById("net.minecraft.client.gui.GuiButton");
     var buttonConstructor = buttonClass.constructors.find(x => x.length === 6);
     
-    var mainButton = buttonConstructor(1000, 10, 10, 150, 20, ModAPI.util.str("Open Menu"));
-    
-    // Hook into the Options GUI initialization
+    var originalInitGui = ModAPI.hooks.methods[ModAPI.util.getMethodFromPackage("net.minecraft.client.gui.GuiOptions", "initGui")];
+
     ModAPI.hooks.methods[ModAPI.util.getMethodFromPackage("net.minecraft.client.gui.GuiOptions", "initGui")] = function (...args) {
+        originalInitGui.apply(this, args); // Ensure the original GUI elements load
+
         var gui = ModAPI.util.wrap(args[0]).getCorrective();
+        var mainButton = buttonConstructor(1000, gui.width - 110, 10, 100, 20, ModAPI.util.str("Open Menu"));
         gui.buttonList.add(mainButton);
     };
-    
+
     // Hook into button click event
     ModAPI.hooks.methods[ModAPI.util.getMethodFromPackage("net.minecraft.client.gui.GuiOptions", "actionPerformed")] = function (button) {
         if (button.id === 1000) {
@@ -21,10 +23,11 @@ function createMainButton() {
 
 // Custom menu with two buttons
 function CustomMenu() {
+    var buttonClass = ModAPI.reflect.getClassById("net.minecraft.client.gui.GuiButton");
+    var buttonConstructor = buttonClass.constructors.find(x => x.length === 6);
+
     this.initGui = function () {
-        var buttonClass = ModAPI.reflect.getClassById("net.minecraft.client.gui.GuiButton");
-        var buttonConstructor = buttonClass.constructors.find(x => x.length === 6);
-        
+        var gui = ModAPI.util.wrap(this).getCorrective();
         this.buttonList.add(buttonConstructor(2000, 10, 10, 100, 20, ModAPI.util.str("Button 1")));
         this.buttonList.add(buttonConstructor(2001, 10, 40, 100, 20, ModAPI.util.str("Button 2")));
     };
